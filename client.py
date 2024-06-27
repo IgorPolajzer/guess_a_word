8654d5b0aaf6bb2fa0d4b4a65437757339cf4531
igork <igor.polajzer@student.sum.si>
2024-06-27 16:01:43 +0200

    Implemented basic socket connection

---
+++ client.py
@@ -0,0 +1,14 @@
+import socket
+
+server_ip = '127.0.0.1'
+server_port = 12345
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect((server_ip, server_port))
+
+message = "Hello, Server!"
+client_socket.send(message.encode())
+data = client_socket.recv(1024)
+print(f"Received: {data.decode()}")
+
+client_socket.close()
--- server.py
+++ server.py
@@ -3,4 +3,19 @@ import socket
 server_ip = '127.0.0.1'
 server_port = 12345
 
+max_connections = 5
+
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind((server_ip, server_port))
+server_socket.listen(max_connections)
+
+client_socket, client_address = server_socket.accept()
+
+
+data = client_socket.recv(1024)
+client_socket.send("Hello, Client!".encode())
+
+client_socket.close()
+server_socket.close()
+
+
